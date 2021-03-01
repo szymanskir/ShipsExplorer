175ee2ed09593400da4aa300e70bd05650be7909
szymanskir <ryszard.szymanski@outlook.com>
2021-03-01 22:18:07 +0100

    Add ship selection module

--- app.R
+++ app.R
@@ -1,12 +1,15 @@
+library(sass)
 library(shiny.semantic)
 
+ships_data_manager <- ShipsDataManager$new(data.table::fread("data/ships.csv"))
+
 ui <- semanticPage(
-  card(
-    div(class = "content", "ShinyExplorer")
-  )
+  includeCSS("www/styles.css"),
+  ship_selection_menu_ui("ship-selection")
 )
 
 server <- function(input, output, session) {
+  ship_selection_data <- ship_selection_menu_server("ship-selection", ships_data_manager)
 }
 
 shinyApp(ui, server)
