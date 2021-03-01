c28a0cb79cba9ea1ac07d20c7191807cb447869c
szymanskir <ryszard.szymanski@outlook.com>
2021-03-01 19:33:56 +0100

    Add shiny.semantic

--- app.R
+++ app.R
@@ -1,35 +1,12 @@
-ui <- fluidPage(
-  # ======== Modules ========
-  # exampleModuleUI is defined in R/example-module.R
-  wellPanel(
-    h2("Modules example"),
-    exampleModuleUI("examplemodule1", "Click counter #1"),
-    exampleModuleUI("examplemodule2", "Click counter #2")
-  ),
-  # =========================
+library(shiny.semantic)
 
-  wellPanel(
-    h2("Sorting example"),
-    sliderInput("size", "Data size", min = 5, max = 20, value = 10),
-    div("Lexically sorted sequence:"),
-    verbatimTextOutput("sequence")
+ui <- semanticPage(
+  card(
+    div(class = "content", "ShinyExplorer")
   )
 )
 
 server <- function(input, output, session) {
-  # ======== Modules ========
-  # exampleModuleServer is defined in R/example-module.R
-  exampleModuleServer("examplemodule1")
-  exampleModuleServer("examplemodule2")
-  # =========================
-
-  data <- reactive({
-    # lexical_sort from R/example.R
-    lexical_sort(seq_len(input$size))
-  })
-  output$sequence <- renderText({
-    paste(data(), collapse = " ")
-  })
 }
 
 shinyApp(ui, server)
