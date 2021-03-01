224fea1e0da5e4fca141d6c14cd346b120a09628
szymanskir <ryszard.szymanski@outlook.com>
2021-03-01 20:22:05 +0100

    Add ship data manager for handling ship data

---
+++ R/ships_data_manager.R
@@ -0,0 +1,20 @@
+ShipsDataManager <- R6Class(
+  classname = "ShipsDataManager",
+  
+  private = list(
+    ship_data = NA
+  ),
+  public = list(
+    initialize = function(ship_data) {
+      private$ship_data <- ship_data
+    },
+    
+    get_ship_types = function() {
+      unique(private$ship_data$ship_type) 
+    },
+    
+    get_ships_of_given_type = function(type) {
+      unique(private$ship_data[ship_type == type, SHIPNAME])
+    }
+  )
+)
