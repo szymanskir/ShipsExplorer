a6f0086cacc2bba7a15f4c0ce7d61898766b225c
szymanskir <ryszard.szymanski@outlook.com>
2021-03-03 19:16:41 +0100

    Fix tests

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -1,10 +1,12 @@
 test_that("Server e2e reactive graph smoke test", {
   withr::local_envvar(R_CONFIG_ACTIVE = "test")
   
-  expect_silent({
     testServer(expr = {
-      session$setInputs(`ship_selection-ship_type_dropdown` = "Cargo")
-      session$setInputs(`ship_selection-ship_name_dropdown` = "KAROLI")
+      expect_silent({
+        session$setInputs(`ship_selection-ship_type_dropdown` = "Cargo")
+        session$setInputs(`ship_selection-ship_name_dropdown` = "KAROLI")
+        
+        expect_true(!is.null(ship_route()))
+      })
     })
-  })
 })
